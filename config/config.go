adcbae831ed1f8566d1094dd528bc15f41ed9fb1
shinedi <shinedi@foxmail.com>
2021-01-25 22:43:06 +0800

    update

--- config/config.go
+++ config/config.go
@@ -15,6 +15,31 @@ import (
 	"os"
 )
 
+func SetEnv(env string) {
+	_ = os.Setenv("ENVIRON", env)
+}
+func GetEnv() string {
+	return os.Getenv("ENVIRON")
+}
+
+func SetLocation(location string) {
+	_ = os.Setenv("LOCATION", location)
+}
+func GetLocation() string {
+	return os.Getenv("LOCATION")
+}
+
+func IsLocal() bool {
+	if GetLocation() == "local" {
+		return true
+	}
+	return false
+}
+
+var (
+	Config *Yaml
+)
+
 type Yaml struct {
 	Mysql *mysql.Config `json:"mysql" yaml:"mysql"`
 	Mongo *mongo.Config `json:"mongo" yaml:"mongo"`
@@ -22,7 +47,7 @@ type Yaml struct {
 }
 
 // 环境加后缀
-func ParseConfig() *Yaml {
+func init() {
 	name := ""
 	if IsLocal() {
 		name = "config/" + GetEnv() + "-" + name + ".yaml"
@@ -33,7 +58,25 @@ func ParseConfig() *Yaml {
 	if err := util.ParseYaml(name, config); err != nil {
 		panic(fmt.Sprintf("解析配置文件错误: %v", err.Error()))
 	}
-	return config
+	Config = config
+}
+
+func init() {
+	switch GetEnv() {
+	case RELEASE:
+		REDIS_ADDRESS = HOST_RELEASE + ":63379"
+		MYSQL_HOST = HOST_RELEASE + ":33336"
+		MONGO_ADDRESS = HOST_RELEASE + ":27020"
+		OSS_ADDRESS = "http://" + OSS_RELEASE
+	case LOCAL:
+		REDIS_ADDRESS = HOST_LOCAL + ":63379"
+		MYSQL_HOST = HOST_LOCAL + ":33336"
+		MONGO_ADDRESS = HOST_LOCAL + ":27020"
+		OSS_ADDRESS = "http://" + OSS_LOCAL
+	default:
+		//panic("启动环境错误")
+
+	}
 }
 
 const (
@@ -73,45 +116,6 @@ var (
 	//oss
 	OSS_ADDRESS string
 )
-
-func SetEnv(env string) {
-	_ = os.Setenv("ENVIRON", env)
-}
-func GetEnv() string {
-	return os.Getenv("ENVIRON")
-}
-
-func SetLocation(location string) {
-	_ = os.Setenv("LOCATION", location)
-}
-func GetLocation() string {
-	return os.Getenv("LOCATION")
-}
-
-func IsLocal() bool {
-	if GetLocation() == "local" {
-		return true
-	}
-	return false
-}
-
-func init() {
-	switch GetEnv() {
-	case RELEASE:
-		REDIS_ADDRESS = HOST_RELEASE + ":63379"
-		MYSQL_HOST = HOST_RELEASE + ":33336"
-		MONGO_ADDRESS = HOST_RELEASE + ":27020"
-		OSS_ADDRESS = "http://" + OSS_RELEASE
-	case LOCAL:
-		REDIS_ADDRESS = HOST_LOCAL + ":63379"
-		MYSQL_HOST = HOST_LOCAL + ":33336"
-		MONGO_ADDRESS = HOST_LOCAL + ":27020"
-		OSS_ADDRESS = "http://" + OSS_LOCAL
-	default:
-		//panic("启动环境错误")
-
-	}
-}
 
 func GetRedisConfig(database int) *redis.Config {
 	return &redis.Config{
