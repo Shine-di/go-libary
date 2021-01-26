762b366a281db17e0b3663118d89242bea020c5f
shinedi <shinedi@foxmail.com>
2021-01-26 09:56:03 +0800

    update

--- node/option.go
+++ node/option.go
@@ -7,12 +7,31 @@ package node
 
 import "github.com/dishine/libary/job"
 
+type Options struct {
+	Options []OptionFunc
+	Jobs    []job.Job // 定时任务
+}
+
 type OptionFunc interface {
 	GetOrder() Order
 	GetOptionFunc() Func
 }
 
-type Options struct {
-	Options []OptionFunc
-	Jobs    []job.Job // 定时任务
+type DefaultOptionFunc struct {
+	order      Order
+	optionFunc Func
+}
+
+func NewOptionFunc(order Order, optionFunc Func) *DefaultOptionFunc {
+	return &DefaultOptionFunc{
+		order:      order,
+		optionFunc: optionFunc,
+	}
+}
+
+func (o *DefaultOptionFunc) GetOrder() Order {
+	return o.order
+}
+func (o *DefaultOptionFunc) GetOptionFunc() Func {
+	return o.optionFunc
 }
