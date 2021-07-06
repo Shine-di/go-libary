abbf5d82ff8042cb5a1c5f7564a1b911bda17165
shinedi <shinedi@foxmail.com>
2021-07-06 10:59:57 +0800

    update

--- token/token.go
+++ token/token.go
@@ -29,9 +29,19 @@ const (
 	N_TEST_USER_2 = "v5vB3h74Tptd9k16Gs84qaeQQMuQYqOk7SrA3Dfu5Lsm7v7bb9"
 	N_TEST_USER_3 = "orxSUKhb7pLg4drzLMXV9TMmRsOCk1KO2vnz4ZppmBlnwfHqmf"
 	N_TEST_USER_4 = "q8vQZXx3xeds35Zj3uaewQFjpsw8BUWcOHKqlhVBaG1N9mnaPC"
+
+	//20210706 更新
+	N_TEST_USER_5 = "t9uNipkLF3XYEyPh37s8MPUfTPQt9sE0jQKANmpiafD9AJBntT"
+	N_TEST_USER_6 = "QwHdDkUY74VSauoHLNmsud4LVZJ8pzk8t1CiweoIV1illqZRfU"
+	N_TEST_USER_7 = "4PXo7Bm4HQlLrOzI4RtuQbu616jjpqxJPWtME3lRqg3aqUdFJ9"
+	N_TEST_USER_8 = "MqlqZ28IJTBw4o7llkOvdU33ulEiAdd7CscUDHUtH4h0FtrxI2"
+	N_TEST_USER_9 = "3QRlMRyFK1yEAZhsnFrLZayVRjAHcSo1cvcKiWoJfixyxcrGEt"
 )
 
 var (
+	tokenMap = map[string][]gameAuth{}
+)
+func init()  {
 	tokenMap = map[string][]gameAuth{
 		TEST: {
 			LOL,
@@ -40,42 +50,33 @@ var (
 			KOG,
 			RATE,
 		},
-		//USER_ONE: {
-		//	//LOL,
-		//	//DOTA,
-		//	//CSGO,
-		//	//KOG,
-		//},
-		//TEST_USER_1: {
-		//	//LOL,
-		//	//DOTA,
-		//	//CSGO,
-		//	//KOG,
-		//},
-		//TEST_USER_2: {
-		//	//LOL,
-		//	//DOTA,
-		//	//CSGO,
-		//	//KOG,
-		//},
-		//
 		N_TEST_USER_1: {
 			RATE,
 		},
-		N_TEST_USER_2: {
-			RATE,
-		},
+		//N_TEST_USER_2: {
+		//	RATE,
+		//},
 		N_TEST_USER_3: {
 			RATE,
 		},
-		N_TEST_USER_4: {
+		N_TEST_USER_5: {
 			RATE,
 		},
-		//N_TEST_USER_6: {
-		//	RATE,
-		//},
+		N_TEST_USER_6: {
+			RATE,
+		},
+		N_TEST_USER_7: {
+			RATE,
+		},
+		N_TEST_USER_8: {
+			RATE,
+		},
+		N_TEST_USER_9: {
+			RATE,
+		},
+
 	}
-)
+}
 
 func TokenMiddleware(game gameAuth) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -108,3 +109,24 @@ func auth(game gameAuth, auths []gameAuth) bool {
 	}
 	return false
 }
+
+//func SyncToken( enum redis.PrefixEnum) error {
+//	go func() {
+//		for {
+//			r := redis.GetRedis()
+//			if r == nil {
+//				log.Warn("连接redis错误")
+//				continue
+//			}
+//			key := fmt.Sprintf("HTTP-APT-TOKEN")
+//			d,err := r.GetValue(enum,key)
+//			if err != nil {
+//				log.Info("同步token错误",zap.Error(err))
+//				continue
+//			}
+//
+//			<- time.After(time.Minute)
+//		}
+//	}()
+//	return nil
+//}
